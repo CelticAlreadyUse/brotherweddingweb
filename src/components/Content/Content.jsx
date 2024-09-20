f99cadfa93b24431865dc9cebe2f3c96e204c61d
CelticAlreadyUse <wahyusantosokanisius@gmail.com>
2024-09-20 12:47:14 +0700

    try

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,10 +3,11 @@ import './content.css'
 const Content = () => {
   return (
     <section className='background-image-bride relative h-screen w-screen '>
-    <main className='ml-4 absolute box-main w-fit h-full place-content-end bottom-0'>
+    <main className='ml-4 absolute grid  bottom-0 box-main w-svh place-items-start h-full place-content-end'>
         <div className='rectangle1 mb-[-10px] ml-[-10px]'>
         <h1 className="wedding-of-title ">Wedding of</h1>
         </div>
+
         <h1 className='heading-title mb-[-20px]'>Yuda</h1>
           <p className=' text-[#e79238] text-[20px] text-center  w-full'>&</p>
         <h1 className='heading-title mt-[-40px]'>Astri</h1>
